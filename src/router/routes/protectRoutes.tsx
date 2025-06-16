f3be89ca365edd5ba54415f83229594d90190d14
shubham <shubham@gmail.com>
2025-06-16 16:14:23 +0530

    add change

--- src/router/routes/protectRoutes.tsx
+++ src/router/routes/protectRoutes.tsx
@@ -1,37 +1,21 @@
-import React, { useEffect, useState } from "react";
+// src/components/ProtectedRoute.tsx
+import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "../../redux/store";
-import { user_info } from "../../redux/reducers/authReducer";
 
 interface ProtectRouteProps {
   allowedRoles: string[];
+  userRole: string | null;
   children: React.ReactElement;
 }
 
-const ProtectRoutes = ({ allowedRoles, children }: ProtectRouteProps) => {
-  const dispatch = useDispatch<AppDispatch>();
+const ProtectRoutes = ({ allowedRoles, userRole, children }: ProtectRouteProps) => {
   const location = useLocation();
-
-  const { userInfo, loader } = useSelector((state: RootState) => state.auth);
-  const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem("accessToken");
-  //   if (!userInfo && token) {
-  //     dispatch(user_info(token)).finally(() => setLoading(false));
-  //   } else {
-  //     setLoading(false);
-  //   }
-  // }, [dispatch, userInfo]);
-
-  // if (loading || loader) return <div className="text-center mt-10">Loading...</div>;
-
-  if (!userInfo) {
+  
+  if (!userRole) {
     return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
-  if (!allowedRoles.includes(userInfo.role)) {
+  if (!allowedRoles.includes(userRole)) {
     return <Navigate to="/unauthorized" replace />;
   }
 
